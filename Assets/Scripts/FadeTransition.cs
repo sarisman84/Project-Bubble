8d528a0d7b0b5a63fdef73db8e0fea7655372992
Dejan <57526262+dejansavic32@users.noreply.github.com>
2019-11-08 12:46:31 +0100

    Scene Switch
    Delayed scene switch with fade in/out transition.

---
+++ Assets/Scripts/FadeTransition.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeTransition : MonoBehaviour
+{
+    public float effectDuration;
+    public bool ignoreTimeScale;
+    public bool fadeInOnSceneLoad;
+    private Image image;
+
+    private void Start()
+    {
+        image = GetComponent<Image>();
+        if (fadeInOnSceneLoad)
+        {
+            image.CrossFadeAlpha(0, effectDuration, ignoreTimeScale);
+        }
+    }
+
+    public void Fade(bool fadeIn)
+    {
+        if (fadeIn)
+        {
+            image.CrossFadeAlpha(0, effectDuration, ignoreTimeScale);
+        }
+        else
+        {
+            image.CrossFadeAlpha(1, effectDuration, ignoreTimeScale);
+        }
+    }
+}
---
+++ Assets/Scripts/SceneSwitch.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitch : MonoBehaviour
+{
+    public float delay;
+    public void SwitchScene(string sceneName) //loads given scene
+    {
+        StartCoroutine("DelayedSceneSwitch", sceneName);
+    }
+
+    IEnumerator DelayedSceneSwitch(string sceneName) //applies delay
+    {
+        yield return new WaitForSeconds(delay);
+        SceneManager.LoadScene(sceneName);
+    }
+}
