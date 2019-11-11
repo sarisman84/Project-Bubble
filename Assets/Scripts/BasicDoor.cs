31172a5929048ef0ac790db8c740ed02fe51e4cb
Dejan <57526262+dejansavic32@users.noreply.github.com>
2019-11-11 15:07:20 +0100

    Key/Password/Lock
    Created a combination of inventory key items, passwords, locks and doors that work with each other.

---
+++ Assets/Scripts/BasicDoor.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicDoor : MonoBehaviour, IInteractable
+{
+    public Lock doorLock;
+
+    private Animator animator;
+    private bool doorIsOpen;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public bool InteractWith()
+    {
+        if (doorLock.unlocked && !doorIsOpen)
+        {
+            animator.SetBool("open", true);
+            doorIsOpen = true;
+        }
+        else if (doorIsOpen)
+        {
+            animator.SetBool("open", false);
+            doorIsOpen = false;
+        }
+        return false;
+    }
+
+    public string MessageOnDetection()
+    {
+        return "Open Door";
+    }
+}
