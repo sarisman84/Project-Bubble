07764630a180a92049275c240a685ab600a1d161
erikneuhofer <56161962+erikneuhofer@users.noreply.github.com>
2019-11-12 12:08:34 +0100

    OnOffSwitch
    Switch script that enables and disables set objects when interacted with

---
+++ Assets/Scripts/OnOffSwitch.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+// Erik Neuhofer
+public class OnOffSwitch : MonoBehaviour, IInteractable
+{
+    [SerializeField] GameObject[] objectsToTurnOf = null; // Set objects to be affected in Inspector
+    private string whatToSay = "Interact";       
+
+    public bool CanBeInteractedWith()
+    {
+        if (objectsToTurnOf == null)
+        {
+            Debug.LogWarning("No references made in Inspector!");
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+
+    public bool InteractWith()
+    {
+        if (CanBeInteractedWith() == true)
+        {
+            foreach (GameObject obj in objectsToTurnOf)
+            {
+                obj.SetActive(!obj.activeSelf);
+            }
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public string MessageOnDetection()
+    {
+        return whatToSay;
+    }
+
+    public void EndInteration() { }
+}
