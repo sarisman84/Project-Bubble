5d8f13a09f49afc11d0c5f8a78729491cd95612c
Gabeh123 <gabriel.henningsohn@outlook.com>
2019-11-20 09:29:44 +0100

    Scene 1
    Scene 1. NOT DONE

---
+++ Assets/Scripts/SleepCheck.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SleepCheck : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject gilad;
+    [SerializeField]
+    public GameObject buttons;
+    bool canISleep = false;
+
+    void Update()
+    {
+        //if () //INSERT "IS QUEST COMPLETE" HERE TO MAKE canISleep TRUE!
+        //{
+        //    canISleep = true;
+        //}
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (canISleep == true && other.gameObject.CompareTag("Player"))
+        {
+            GameManager.Instance().SetFPSInput(false);
+            Cursor.lockState = CursorLockMode.None;
+            EnableButtons();
+        }
+    }
+
+    //public bool CanISleep()
+    //{
+    //    for (int i = 0; i < QuestLog.Instance().quests.Count; i++)
+    //    {
+    //        if ()
+    //    }
+    //}
+
+    public void EnableButtons()
+    {
+        buttons.SetActive(true);
+    }
+
+    public void DisableButtons()
+    {
+        buttons.SetActive(false);
+        GameManager.Instance().SetFPSInput(true);
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+}
