1d7ccfa39d5dc56c9a4795fc3f79ad08282ec04a
Simon Voss <woss.simon@gmail.com>
2019-11-20 10:08:56 +0100

    Main menu 2.0 + swicthes scenes

--- Assets/Scripts/Items/USBItem.cs
+++ Assets/Scripts/Items/USBItem.cs
@@ -5,16 +5,19 @@ using UnityEngine;
 public class USBItem : MonoBehaviour, IInteractable //Dejan, usb 3d object
 {
     [SerializeField] SceneSwitch sceneSwitch = null; //a reference to the scene switch prefab (Fade In_Out)
+    [SerializeField] int buildIndex = 0;
+
+    [SerializeField] string message = "";
 
     public bool InteractWith() //executes when interacted with, changes scene to main menu
     {
-        sceneSwitch.SwitchScene("Main Menu");
+        sceneSwitch.SwitchScene(buildIndex);
         return false;
     }
 
     public string MessageOnDetection() //displays name on hover
     {
-        return "USB";
+        return message;
     }
 
     public bool CanBeInteractedWith() //can always be interacted with
