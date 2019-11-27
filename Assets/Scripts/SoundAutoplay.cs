72af54241e60a980a94ec147820a2f35be819bba
Dejan <57526262+dejansavic32@users.noreply.github.com>
2019-11-27 11:30:15 +0100

    Cutscene sound

---
+++ Assets/Scripts/SoundAutoplay.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundAutoplay : MonoBehaviour
+{
+    [SerializeField] List<AudioClip> sounds = new List<AudioClip>();
+    [SerializeField] float interval;
+
+    AudioSource audioSource;
+    float nextTime;
+
+    private void Start()
+    {
+        audioSource = gameObject.AddComponent<AudioSource>();
+    }
+
+    private void Update()
+    {
+        if (Time.time > nextTime)
+        {
+            audioSource.PlayOneShot(sounds[Random.Range(0, sounds.Count - 1)]);
+            nextTime = Time.time + interval;
+        }
+    }
+}
