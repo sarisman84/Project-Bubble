fe5f9f8078f0daca1fb153d9f7f015c3daab8e2f
Dejan <57526262+dejansavic32@users.noreply.github.com>
2019-11-29 12:48:10 +0100

    Scene 1 Updated

---
+++ Assets/Scripts/GiladAnimations.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GiladAnimations : MonoBehaviour
+{
+    [SerializeField] Guard guard = null;
+    [SerializeField] Animator animator = null;
+
+    private void Update()
+    {
+        if (guard.playerdUncovered)
+        {
+            animator.SetBool("talking", true);
+        }
+        else
+        {
+            animator.SetBool("talking", false);
+        }
+    }
+}
